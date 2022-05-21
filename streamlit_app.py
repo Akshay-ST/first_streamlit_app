c9b6cf2d41352236792c26a2c8ee536a70d12bcd
Akshay Singh Thakur <th.akshay.singh@outlook.com>
2022-05-21 12:31:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ s.text('🥑🍞 Avocado Toast')
 s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("fruit_macros.txt")
 s.dataframe(my_fruit_list)
 
