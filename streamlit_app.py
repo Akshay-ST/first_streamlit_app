e3dda774cc723296acf3f26ff2f510ac1a4acefc
Akshay Singh Thakur <th.akshay.singh@outlook.com>
2022-05-21 12:20:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as s
 
 s.title('My Parents New Healthy Diner')
 
-s.header('Breakfast Menu')
-s.text('Omega 3 & Blueberry Oatmeal')
-s.text('Kale, Spinach & Rocket Smoothie')
-s.text('Hard-Boiled Free-Range')
+s.header('Breakfast FAvourites')
+s.text('🥣 Omega 3 & Blueberry Oatmeal')
+s.text('🥗 Kale, Spinach & Rocket Smoothie')
+s.text('🐔 Hard-Boiled Free-Range')
+s.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
