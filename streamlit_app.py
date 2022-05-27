69e94a3f9bd68a17752462f4a18c29ad01dbac18
Akshay Singh Thakur <th.akshay.singh@outlook.com>
2022-05-27 20:45:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ s.dataframe(fruityvice_normalized)
 
 
 import snowflake.connector as sf
-my_cnx = sf.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = sf.connector.connect(**s.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
