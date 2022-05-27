7beb8663a27e6bd01fb3dcf21256c8fd50d7bfd7
Akshay Singh Thakur <th.akshay.singh@outlook.com>
2022-05-27 22:33:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 s.header("The fruit list contains:")
 s.dataframe(my_data_row)
+
+
+add_my_fruit = s.text_input('What fruit would you like to add?')
+my_cur.execute("insert into fruit_load_list ( FRUIT_NAME ) values "+add_my_fruit)
+s.write('Thanks for adding ',add_my_fruit)
