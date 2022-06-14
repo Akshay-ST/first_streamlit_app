1932e69c8e696a72f882dff3fbc0e1e7a512e10c
Akshay Singh Thakur <th.akshay.singh@outlook.com>
2022-06-14 18:24:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as s
 import pandas as pd
+import requests as r
+import snowflake.connector as sf
+from urllib.error import URLError as u
 
 s.title('My Parents New Healthy Diner')
-
 s.header('Breakfast FAvourites')
 s.text('🥣 Omega 3 & Blueberry Oatmeal')
 s.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -17,7 +19,6 @@ my_fruit_list = pd.read_csv("fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #s.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-
 fruits_selected = s.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
@@ -29,7 +30,7 @@ s.header('Fruityvice Fruit Advice!')
 fruit_choice = s.text_input('What fruit would you like information about?', 'Kiwi')
 s.write('The user entered', fruit_choice)
 
-import requests as r
+
 fruityvice_response = r.get("https://Fruityvice.com/api/fruit/" + fruit_choice)
 #s.text(fruityvice_response.json()) 
 
@@ -37,7 +38,6 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 s.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector as sf
 my_cnx = sf.connect(**s.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -46,7 +46,8 @@ my_data_row = my_cur.fetchall()
 s.header("The fruit list contains:")
 s.dataframe(my_data_row)
 
-
+s.stop() 
+  
 add_my_fruit = s.text_input('What fruit would you like to add?')
 if (add_my_fruit != ''):
   input_qur = "insert into fruit_load_list values (\'" + add_my_fruit + "\');"
